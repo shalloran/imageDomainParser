d172cdb5fc4a9db1d8b94aa3ee93dd0d04a80ef0
Sean Halloran <sean@halloran.io>
2023-11-19 16:30:26 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Domain Parser
+
+This Python script extracts domain names from images using Optical Character Recognition (OCR).
+
+## Description
+
+The script prompts the user for a directory path, then processes all .png and .jpg images in that directory. It uses the OpenCV library to load and convert each image to grayscale, then uses the pytesseract library (a Python wrapper for Google's Tesseract-OCR Engine) to perform OCR on the image and extract text.
+
+The script then uses regular expressions to find domain names in the extracted text. It filters and cleans the domain names, ensuring subdomains are also captured. The script removes duplicates from the list of domains, sorts the unique domains alphabetically, and writes them to a .csv file.
+
+## Dependencies
+
+This script requires the following Python libraries:
+
+- OpenCV (cv2)
+- pytesseract
+- glob
+- os
+- csv
+- tqdm
+
+You can install these libraries using pip:
+
+\ bash
+pip install opencv-python pytesseract glob2 tqdm
+\
+
+Note: The script also requires the Tesseract-OCR Engine to be installed on your system. You can download it from [here](https://github.com/tesseract-ocr/tesseract/wiki).
+
+## Usage
+
+Run the script in your terminal:
+
+\ bash
+python domain-parser.py
+\
+
+When prompted, enter the path to the folder containing the images and the path and name for the output .csv file.
---
+++ domain-parser.py
@@ -0,0 +1,59 @@
+# For most python users, you will likely have to:
+# pip install cv2, pytesseract
+
+# 19 seconds for 35 png images
+
+import cv2
+import pytesseract
+import re
+import csv
+import os
+import glob
+from tqdm import tqdm
+
+# GLOBALS
+DOMAINS = []
+PATH_TO_FOLDER = input("Please enter the path to the folder containing the images: ")
+OUTPUT_FILE_NAME = input("Please enter the path and name for the output .csv file: ")
+
+# Function to read image and extract domains
+def domainExtractor(imagePath):
+    # Load the image using cv2
+    img = cv2.imread(imagePath)
+
+    # Convert the image to gray scale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Use tesseract to do OCR on the image
+    text = pytesseract.image_to_string(gray)
+
+    # Use regex to find domains in the text
+    found_domains = re.findall(r'[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', text)
+    
+    # Filter and clean domain names, ensuring subdomains are also captured
+    for domain in found_domains:
+        # Basic cleaning to ensure it's a proper domain (subdomain.example.com)
+        # This may exclude some valid domains but ensures quality of the domains extracted
+        if '.' in domain and len(domain.split('.')) > 2:
+            DOMAINS.append(domain.lower())
+
+# Use glob to get all png and jpg images in the folder
+imagePaths = glob.glob(os.path.join(PATH_TO_FOLDER, "*.png")) + glob.glob(os.path.join(PATH_TO_FOLDER, "*.jpg"))
+
+# Process each image
+for path in tqdm(imagePaths):
+    domainExtractor(path)
+
+# Remove duplicates from the list of domains
+unique_domains = list(set(DOMAINS))
+
+# Sort the unique domains alphabetically
+sorted_unique_domains = sorted(unique_domains)
+
+# Output the sorted unique domains
+sorted_unique_domains
+
+with open(OUTPUT_FILE_NAME, 'w', newline='') as file:
+    writer = csv.writer(file)
+    for domain in sorted_unique_domains:
+        writer.writerow([domain])
